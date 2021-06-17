b254e720843445a36f467887f69b3287da5cd5aa
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-17 13:57:35 -0700

    minor changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
 * main - Write '"Programming is like building a multilingual puzzle'
 *Return: Always 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
