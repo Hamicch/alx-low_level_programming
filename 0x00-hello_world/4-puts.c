81561d20b85277d1bc836bc6432af89bc3adae59
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-17 13:38:59 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
