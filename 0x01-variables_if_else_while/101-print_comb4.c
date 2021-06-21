3113ce3a6ee73603cd7c737834067581275af429
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-21 05:23:34 -0700

    print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of three digits
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int j;
+
+	for (i = 48; n < 58; i++)
+	{
+		for (n = 48; n < 58; j++)
+		{
+			if (n > i)
+			{
+				for (j = 48; j < 58; j++)
+				{
+					if (j > n)
+					{
+					putchar(i);
+					putchar(n);
+					putchar(j);
+					if (i == 55 && n == 56 && j == 57)
+					{
+						break;
+					}
+
+					putchar(',');
+					putcar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return;
+}
