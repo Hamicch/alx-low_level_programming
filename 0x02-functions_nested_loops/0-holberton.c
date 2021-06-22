165be8009d0b3722d574cb099ccd8fe28c0f28cd
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-22 12:53:05 -0700

    print 'Holberton'

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Print "Holberton"
+ *
+ * Return: Always0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char name_of_school[] = "Holberton";
+
+	while (i < 9)
+	{
+		_putchar(school_name[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
