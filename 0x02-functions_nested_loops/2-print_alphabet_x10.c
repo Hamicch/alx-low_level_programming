6fc1bcbe18774879efbf07d81d3523139f3946c0
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-23 11:46:19 -0700

    print alphabet 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,19 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	char i;
-	
+	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		char c;
 
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z' c++)
 		{
 			_putchar(c);
 		}
