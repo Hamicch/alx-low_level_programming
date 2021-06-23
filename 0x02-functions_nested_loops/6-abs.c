b6b166499d0519c111c120b9057cbd30d5740b4c
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-23 15:03:51 -0700

    print absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "holberton.c"
+
+/**
+ * _abs - Prints the absolute value of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+	int a;
+	
+	a = _abs(-1);
+	_putchar(a);
+
+	a = _abs(0);
+	_putchar(a);
+
+	a = _abs(1);
+	_putchar(a);
+
+	a = _abs(-98);
+	_putchar(a);
+
+	return (0);
+}
