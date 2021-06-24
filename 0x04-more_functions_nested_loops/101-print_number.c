d95fd0b94a17d323d3aca26549dc09d249479ab6
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-24 13:40:43 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_number - Prints an integer.
+ *
+ * @n: The integer to print.
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+}
