5027c30a5d886f2735dc45843fae976e2b02ef16
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-23 17:20:03 -0700

    add to numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 
 /**
- * add - Sums two integers
+ * add - Sums two integers and
+ * return the result.
  *
- * Return: Sum of the two intgers
+ * @num1: integer one.
+ * @num2: integer two.
+ *
+ * Return: Sum of the two integers
  */
 
 int add(int, int)
 {
 	int num1;
 	int num2;
+
 	sum = num1 + num2;
 
 	return sum;
