16f635a92348ab8594360bf4f0505eb41d4d7c0d
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-24 00:22:48 -0700

    print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints numbers from 'n' to 98.
+ * print_to_98 - Prints numbers from n to 98.
  *
  * @n - The starting point (first number to be printed).
  *
