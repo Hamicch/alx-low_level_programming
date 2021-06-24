d70f72503ec513d43b82f9024b4d31abab862ebb
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-23 17:15:16 -0700

    add to numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - Sums two integers
+ *
+ * Return: Sum of the two intgers
+ */
+
+int add(int, int)
+{
+	int num1;
+	int num2;
+	sum = num1 + num2;
+
+	return sum;
+}
