c7bcff0e56b1dcf8cb74c55f7e21e3415a12c220
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-24 02:08:32 -0700

    Print first 50 numbers of the fibonacci seq

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of fibonacci numbers under 4,000,000
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	long int f1 = 1, f2 = 2, sum = f2, tmp;
+
+	while (f2 < 4000000)
+	{
+		tmp = f1 + f2;
+		f1 = f2;
+		f2 = tmp;
+		sum += (f2 % 2 == 0) ? f2 : 0;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
