fd7c7103629ade7623c6103c9fc528afcdc91412
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-24 00:57:06 -0700

    print 100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 {
 	int a, b, op;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0; && n; <= 15)
 	{
 	for (a = 0; a <= n; a++)
 	{
@@ -26,7 +26,7 @@ void print_times_table(int n)
 		{
 		_putchar(32);
 		_putchar(32);
-		_putchar(op + 48)
+		_putchar(op + 48);
 		}
 		else if (op <= 99)
 		{
@@ -38,7 +38,7 @@ void print_times_table(int n)
 		{
 		_putchar(((op / 100) % 10) + 48);
 		_putchar(((op / 10) % 10) + 48);
-		_putchar(((op % 10) + 48);
+		_putchar((op % 10) + 48);
 		}
 	}
 	_putchar('\n');
