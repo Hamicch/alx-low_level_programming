12d2fa336f3f28cf8b03ae3bf7289996a22f4e60
Hayatu Sanusi <hamichtech@gmail.com>
2021-06-24 12:20:20 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_square - Prints n square according n number of times
+ *
+ * @size: The number of squares/number of times
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+      	{
+		for (a = 0; a < size; a++)
+		{
+			for  (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
