e220d9efc633abe526d9d4109e1c5153a582ed24
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-04 20:02:30 -0700

    reset pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * 0-reset_to_98 - Directs a pointer to an int and changes
+ * and changes the value to 98.
+ *
+ * @n: The variable pointing to the integer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
