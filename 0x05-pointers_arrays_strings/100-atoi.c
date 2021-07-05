39a86107615fa8142f8d5d2ac642120bd4668f69
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-04 23:06:56 -0700

    convert a number in a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 
 	while (s[0] != '\0')
 	{
-		if (s[0] = '-')
+		if (s[0] == '-')
 			sign_cache *= -1;
 		else if (s[0] >= '0' && s[0] <= '9')
 			result = (result * 10) + (s[0] - '0') * sign_cache;
@@ -24,4 +24,6 @@ int _atoi(char *s)
 
 		s++;
 	}
-return (result);
+
+	return (result);
+}
