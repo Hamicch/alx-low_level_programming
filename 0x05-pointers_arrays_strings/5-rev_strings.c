dd318bbdf9693251383093c5f1a78e3b314553f8
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-04 21:32:11 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_strings.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j, right;
+
+	while (s[i])
+		i++;
+
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
+	{
+		char temp s[right];
+		int left = i - right - 1;
+
+		s[right] = s[left];
+		s[left] = temp;
+
+		right++;
+	}
+
+{
+
