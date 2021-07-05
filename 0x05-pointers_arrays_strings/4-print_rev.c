5b00e83a5d2c41d43840be9c2bcec74295c9fe38
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-04 21:16:53 -0700

    print in rerverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s The string to be printed
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
+
+	_putchar('\n');
+}
