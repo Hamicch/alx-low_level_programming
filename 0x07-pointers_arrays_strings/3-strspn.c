cdf6ede1649bf37e2be0931d936211ac1e11587e
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-05 12:52:19 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Get the length of a prefix substring
+ *
+ * @s: The character to print
+ *
+ * @accept: Character
+ *
+ * Return: Number of bytes in first segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+				break;
+			count++;
+			accept++;
+
+			s++;
+		}
+		if (accept == '\0')
+			break;
+	}
+
+	return (count + 1);
+}
