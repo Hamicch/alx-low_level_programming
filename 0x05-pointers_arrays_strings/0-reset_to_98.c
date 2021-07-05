6b65845ae56385680ffaa2125cd11a7926745cdf
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-04 20:05:24 -0700

    commenting update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * 0-reset_to_98 - Directs a pointer to an int and changes
+ * reset_to_98 - Directs a pointer to an int and changes
  * and changes the value to 98.
  *
  * @n: The variable pointing to the integer
