86c6a1cad330f2d76edaa09196750b008a860a70
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-06 07:27:47 -0700

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,18 +13,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int c = 0;
 	int d = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		c = c + *(a + i * sizeof(int));
+		c += a[i];
+		d += a[size - i - 1];
+		a += size;
 	}
-
-	for (j = 0; j < size; j++)
-	{
-		d = d + *(a + (size * j) + (size - j - 1));
-	}
-	printf("%d, %d\n", c, d);
+	printf("%d, ", c);
+	printf("%d\n", d);
 }
