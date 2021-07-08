d7ac82721fa9d05ea8251e72d20cb98715854d0b
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-07 17:42:18 -0700

    print_dev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * @s: Pointer to the string
+ *
+ * Return: void
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_put_rev_recursion(s + 1);
+	_putchar(*s);
+}
