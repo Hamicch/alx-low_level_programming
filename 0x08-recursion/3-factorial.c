aea6c271bf8d576b7232173efeb3f1d8f752eb69
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-08 14:13:40 -0700

    factprial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * factorial - Return the factorial of a given number @n
+ * factorial - Returns the factorial of a given number @n
  *
  * @n: The number
  *
- * return: An integer, the result of @n factorial
+ * Return: An integer, the result of @n factorial
  */
 
 int factorial(int n)
