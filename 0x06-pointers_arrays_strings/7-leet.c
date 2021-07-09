4eb8c157b2af4fbb4fef1af04f8bc0c15fa44626
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-09 05:12:41 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @c: String
+ *
+ * Retutn: String that is encoded
+ */
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', '0', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			/*32 is the difference between lowercase letters and uppercase letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
+}
