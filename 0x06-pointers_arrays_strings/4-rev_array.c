887d13ac693331ce81f6b60bc3a4f545248c1a3e
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-09 04:38:50 -0700

    reverse_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,7 +5,7 @@
  *
  * @a: An array of integers
  *
- * @m: The number of elements to swap
+ * @n: The number of elements to swap
  *
  * Return: Nothing
  */
@@ -14,7 +14,7 @@ void reverse_array(int *a, int n)
 {
 	int i, j, t;
 
-	for (i = 0; j = (n - 1); i < j; i++, j--)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
 		t = a[i];
 		a[i] = a[j];
