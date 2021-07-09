ce84167b4ed831c9fe6dd09641f48846d99a85dc
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-09 04:46:18 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to upper
+ *
+ * @str: String to return
+ *
+ * Return: String
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
