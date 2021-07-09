c3cc7be03de0d8f64e7637d5a56553d35a5523b8
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-09 03:39:34 -0700

    strcar

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,13 +1,16 @@
-void print_number(int n);
-int _putchar(char n);
-char *rot13(char *);
-char *leet(char *);
-char *cap_string(char *);
-char *string_toupper(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *infinite_add(char *n1, char *n2, char *r, size_r);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
