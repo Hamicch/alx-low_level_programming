30d321fc084d3059c0d0e96ced27afe60179cde2
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-12 12:29:13 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * whatsmyname - A program that print its name followed
+ * by a newline
+ *
+ * @argc: Argument character
+ *
+ * @argv: Argument variable
+ *
+ * Return: it's name
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc i++)
+	{
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
+	return 0;
+}
