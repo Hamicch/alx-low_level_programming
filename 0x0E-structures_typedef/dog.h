6a45efd53f81bbe2d1e7cb08f2c3d60f5027cecc
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-19 19:40:15 -0700

    struct 'dog'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,28 @@
 #ifndef DOG_H_
 #define DOG_H_
 
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	int age;
-}
+};
+
+void init_dog(struct dog *d, char *name, char *owner, float age);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, char *owner, int age);
 
 #endif
