80991723fb996ed7e9e6b2ca2943c487a7ae97c6
Hayatu Sanusi <sanusihayatu01@gmail.com>
2021-07-19 20:43:55 -0700

    A function that initialises the strcut variables

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,22 +2,23 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes the dog strcut variables
+ * init_dog - Initializes the dog struct variables
  *
- * @dog: The class
+ * @name: String literal
  *
- * @my_dog: An an instance of @dog class
+ * @d: Pointer to struct dog
  *
- * Return: 0 (Sucess)
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog my_dog;
-
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-
-	return my_dog;
+	if (d)
+	{
+		d->name = name;
+		(*d).age = age;
+		d->owner = owner;
+	}
 }
