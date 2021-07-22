8b433ef5f5e3d83f1bae24d20ac01c931e8f6951
hamicch <hamichtech@gmail.com>
2021-07-22 10:11:51 +0100

    execute function giving as a parameter

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 #define FUNCTION_POINTERS_
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* #ifndef FUNCTION_POINTERS_ */
