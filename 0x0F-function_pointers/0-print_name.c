f91c7d7d1a3a05948bc32f16caf58fd732651bfc
hamicch <hamichtech@gmail.com>
2021-07-23 22:37:33 +0100

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,7 +8,7 @@
  * @f: A callback function that prints @name
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+void print_name(char *array, void (*f)(char *))
 {
 	if (f)
 		f(name);
