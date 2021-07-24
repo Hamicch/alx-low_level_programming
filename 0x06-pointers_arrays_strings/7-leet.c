7c5ce80c8043958e582406da4bb3cb10dbadee24
hamicch <hamichtech@gmail.com>
2021-07-24 18:37:13 +0100

    fixed bug

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,16 +2,14 @@
 
 /**
  * leet - encodes a string into 1337
- *
  * @c: String
- *
  * Retutn: String that is encoded
  */
 
 char *leet(char *c)
 {
 	char *cp = c;
-	char key[] = {'A', 'E', '0', 'T', 'L'};
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
 	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
@@ -19,7 +17,7 @@ char *leet(char *c)
 	{
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			/*32 is the difference between lower case and upper case letters*/
+			/*32 is the difference between lower case letters and apper case letters*/
 			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
@@ -29,4 +27,5 @@ char *leet(char *c)
 	}
 
 	return (cp);
+
 }
