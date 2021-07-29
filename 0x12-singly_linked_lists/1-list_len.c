68e45e2076f48e9deade58b660474440fcb7340e
Hayatu Sanusi <hamichtech@gmail.com>
2021-07-29 22:37:25 +0100

    prints number of elements in a singly list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+include <stdlib.h>
+#inclue "lists.h"
+
+/**
+ * list_len - Calulates the number of elements in a list_t
+ * @h: Pointer to the head node of the list_t
+ * Return: size_t, the number of nodes in a list_t
+ */
+
+size_t print_len(const list_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h)
+	{
+		num_of_nodes++;
+		h = h->next;
+	}
+
+	return (num_of_nodes);
+}
