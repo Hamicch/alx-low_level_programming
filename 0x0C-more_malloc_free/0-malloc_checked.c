af7dd92a863aeaa92209f6f67de95f021c61ad30
hamicch <hamichtech@gmail.com>
2021-08-01 22:29:47 +0100

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to be allocated
+ * Return: pointer to the allocated memory (SUCCESS)
+ * 98 if insufficient memory was available (FAILURE)
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,10 @@
+## More malloc, free
+
+### Learning Objectives
+
+At the end of the project, you are expected to be able to explain to anyone, without the help of Google:
+
+#### General
+
+- How to use the exit function
+- What are the functions callocs and realloc from the standard library and how to use them
