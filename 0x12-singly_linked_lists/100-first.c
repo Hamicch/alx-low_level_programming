32581c72447bc1106dbf4073fe68c434672327c4
hamicch <hamichtech@gmail.com>
2021-08-01 16:26:53 +0100

    first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,9 @@
 
 /**
  * beforeMain - Runs before the main fucntion
- * Return: Returns a string
  */
 
 void __attribute__((constructor)) beforeMain()
 {
-	printf ("You're beat! and yet, you must allow,\n%s",
-			"I bore my house upon my back!\n");
+	printf ("You're beat! and yet, you must allow,\n%s", "I bore my house upon my back!\n");
 }
