969c122787726f0deff8ebdd6632d8b5920fdab5
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-01 11:03:57 +0100

    fixed errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,22 @@
 #include "lists.h"
-#include <stddef.h>
+
+/**
+ *  * _strlen - Returns the length of a string
+ *   * @s: The character containing the string
+ *    * Return: The length of the character
+ *     */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;						               
+		s++;								        }
+
+	return (len);
+}
 
 /**
  * add_node - Adds a new node at the beginning of list_t list
