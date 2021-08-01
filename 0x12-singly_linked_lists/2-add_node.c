a914f64455e7207d13dea679852de45c3efd8f01
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-01 12:45:40 +0100

    fixing errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,10 +17,15 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_head = malloc(sizeof(list_t));
 	if (new_head == NULL)
+	{
 		return (NULL);
+	}
+	else
+	{
 	new_head->str = strdup(str);
 	new_head->len = length;
 	new_head->next = *head;
 	*head = new;
 	return (new_head);
+	}
 }
