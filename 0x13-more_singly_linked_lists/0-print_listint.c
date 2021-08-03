8fd264507440406f16ae5591bb510f1f24405dde
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-03 06:38:30 +0100

    fixing errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,19 +4,20 @@
 
 /**
  * print_listint - Prints all the elements in the listint_t list
- * @h: A node in list_t
+ * @h: Pointer to list_t
  * Return: The number of nodes
  */
 
-size_t print_listint(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t num_of_nodes = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num_of_nodes++;
 		h = h->next;
+		num_of_nodes++;
 	}
+
 	return (num_of_nodes);
 }
