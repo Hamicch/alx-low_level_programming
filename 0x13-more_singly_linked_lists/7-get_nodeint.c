db941f1fda89fe22405b8d2a868b0675fd756ed9
hamicch <hamichtech@gmail.com>
2021-08-03 12:49:10 +0100

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 	listint_t *temp = head;
-
+	
 	while (temp && (counter < index))
 	{
 		temp = temp->next;
