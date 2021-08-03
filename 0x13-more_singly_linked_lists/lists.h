487015eb18de5c66f7a867e8dfe958c2ee9c9e08
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-03 07:44:59 +0100

    task 3

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,5 +19,10 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 
 #endif /* #ifndef MORE_SINGLY_LINKED_LIST_H_ */
