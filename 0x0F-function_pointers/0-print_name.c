45787b5acb6ac5640bb920098176f5e475955f60
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-06 14:21:49 +0100

    fix errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,18 @@
+#include "function_pointers.h"
+
 /**
- * print_name - prints a name
- * @name: pointer to a string
- * @f: a callback function that prints @name
+ * print_name- this functions prints a name
+ * a blank line
+ * @name: string to print.
+ * @f: this a function pointer, that recieves a string.
+ * Description: this function prints a name)?
+ * section header: the header of this function is function_pointers.h)*
+ * Return: this is a void function no return
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
 }
