d417fe2644ad4179c4bb9dbc57b051c0f7aabffd
Hayatu Sanusi <hamichtech@gmail.com>
2021-08-06 14:40:52 +0100

    fixed error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *p;
 
-	if (ptr == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
 
-	return (ptr);
+	return (p);
 }
