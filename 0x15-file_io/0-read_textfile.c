07c210fd01fc5d5d73d217d5709923f7019eda2d
hamicch <hamichtech@gmail.com>
2021-08-10 11:57:38 +0100

    fixed error task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,10 @@
-#include "main.h"
 #include "holberton.h"
-
 /**
- * read_textfile - Reads a text file and prints it to the POSIX stdout
- * @filename: Name of the file that's read
- * @letters: The number of letters read and printed
- * Return: The number of letters it could read and print, 0 if file cannot
- * be printed or read, 0 if @filename is NULL, 0 if write fails or doesn't
- * write the expected amount of bytes
- */
-
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t nletters;
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,16 +1,26 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <elf.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void validate_elf(unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
 
 #endif /* HOLBERTON_H */
