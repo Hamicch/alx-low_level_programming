3d7b7d6d0854f28436d8a1677e43ad934adcf9ce
hamicch <hamichtech@gmail.com>
2021-08-28 12:13:05 +0100

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
 
-	return (largest);
+return (largest);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,11 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
-		day++;
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -24,14 +24,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-	if (month == 2 && day == 60)
+		if (month == 2 && day == 60)
 		{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-	else
+		else
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
